8e491cb2cfef2446ac1aaeb7ad96e97cb17f778e
momokys <778259229@qq.com>
2023-12-15 02:12:40 +0800

    doc: 修改 BtnGroup 文档

--- packages/ui/src/components/btn/stories/btn-group.story.vue
+++ packages/ui/src/components/btn/stories/btn-group.story.vue
@@ -1,11 +1,17 @@
 <template>
   <Story title="Btn/BtnGroup" :layout="{ type: 'grid', width: '100%' }">
-    <Variant>
-      <v-btn-group size="small">
+    <Variant title="playground">
+      <v-btn-group v-bind="state">
         <v-btn label="Left" />
         <v-btn label="Center" />
         <v-btn label="Right" />
       </v-btn-group>
+      <template #controls>
+        <HstSelect title="size" v-model="state.size" :options="['mini', 'small', 'medium', 'large']" />
+        <HstCheckbox title="disabled" v-model="state.disabled" />
+      </template>
+    </Variant>
+    <Variant>
       <v-btn-group size="small">
         <v-btn label="Search" />
         <v-btn icon="Search" />
@@ -19,5 +25,10 @@
 </template>
 
 <script setup lang="ts">
-import { VBtnGroup, VBtn } from '@/components'
+import { VBtnGroup, VBtn, BtnGroupProps } from '@/components'
+import { reactive } from 'vue'
+
+const state = reactive<BtnGroupProps>({
+  size: 'small',
+})
 </script>
