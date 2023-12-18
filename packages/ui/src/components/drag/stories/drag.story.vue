08df43308578b6a8b4b377d2971f64cc1584a97c
momokys <778259229@qq.com>
2023-12-19 00:55:49 +0800

    feat: 开发拖拽功能

---
+++ packages/ui/src/components/drag/stories/drag.story.vue
@@ -0,0 +1,54 @@
+<template>
+  <Story title="Drag" :layout="{ type: 'grid', width: '100%' }">
+    <Variant title="playground">
+      <div style="position: relative; width: 100%; height: 600px">
+        <div ref="tarRef" style="width: 400px; height: 400px; background-color: #00c48f" />
+        <div ref="srcRef" class="box" />
+        <iframe />
+      </div>
+    </Variant>
+  </Story>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const srcRef = ref<HTMLDivElement>()
+const tarRef = ref<HTMLDivElement>()
+onMounted(() => {
+  const srcEl = srcRef.value!
+  const tarEl = tarRef.value!
+  srcEl.draggable = true
+  srcEl.addEventListener('dragstart', (ev) => {
+    console.log(ev.type)
+  })
+  srcEl.addEventListener('drag', (ev) => {
+    console.log(ev.type)
+  })
+  srcEl.addEventListener('dragend', (ev) => {
+    console.log(ev.type)
+  })
+  srcEl.addEventListener('dragenter', (ev) => {
+    console.log(ev.type)
+  })
+  srcEl.addEventListener('dragover', (ev) => {
+    ev.preventDefault()
+    console.log(ev.type)
+  })
+  tarEl.addEventListener('dragleave', (ev) => {
+    console.log(ev.type)
+  })
+  tarEl.addEventListener('drop', (ev) => {
+    console.log(ev.type)
+  })
+})
+</script>
+
+<style scoped lang="scss">
+.box {
+  //position: absolute;
+  width: 100px;
+  height: 100px;
+  background-color: #3a7afe;
+}
+</style>
--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -2,3 +2,12 @@ export interface Point {
   x: number
   y: number
 }
+
+export interface Vec2D {
+  x: number
+  y: number
+}
+
+export interface Vec3D extends Vec2D {
+  z: number
+}
