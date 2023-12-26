c2b802a9929b9a98cc15e2c77fea8cc4f05086c1
momoky <778259229@qq.com>
2023-12-26 17:58:30 +0800

    feat: 开发 scrollbar

--- packages/ui/src/components/scrollbar/props.ts
+++ packages/ui/src/components/scrollbar/props.ts
@@ -3,11 +3,7 @@ import { ExtractPropTypes, PropType } from 'vue'
 export const scrollbarProps = {
   height: [Number, String] as PropType<number | string>,
   maxHeight: [Number, String] as PropType<number | string>,
-  always: Boolean,
-  gap: {
-    type: Boolean,
-    default: true,
-  },
+  embed: Boolean,
 } as const
 
 export type ScrollbarProps = ExtractPropTypes<typeof scrollbarProps>
