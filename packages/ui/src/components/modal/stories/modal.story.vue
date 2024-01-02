9a4742ae7ac8fba6770311202d12b0dc65c6bc82
momoky <778259229@qq.com>
2024-01-02 17:37:49 +0800

    doc: 更新文档

--- packages/ui/src/components/modal/stories/modal.story.vue
+++ packages/ui/src/components/modal/stories/modal.story.vue
@@ -6,32 +6,31 @@
           <h1 style="margin: 0">Hello world!</h1>
         </div>
       </v-modal>
+      <VBtn label="模态窗" @click="state.visible = true" />
       <template #controls>
-        <HstButtonGroup
-          v-model="state.visible"
-          title="visible"
-          :options="[
-            {
-              label: 'true',
-              value: true,
-            },
-            {
-              label: 'false',
-              value: false,
-            },
-          ]"
-        />
+        <HstText v-model="state.title" title="title" />
+        <HstNumber v-model="state.height" title="height" />
+        <HstNumber v-model="state.width" title="width" />
+        <HstCheckbox title="shade" v-model="state.shade" />
+        <HstCheckbox title="fullscreen" v-model="state.fullscreen" />
+        <HstCheckbox title="fullscreenEnabled" v-model="state.fullscreenEnabled" />
+        <HstCheckbox title="draggable" v-model="state.draggable" />
       </template>
     </Variant>
   </Story>
 </template>
 <script setup lang="ts">
 import { reactive } from 'vue'
-import { VModal } from '@/components'
+import { VBtn, VModal, ModalProps } from '@/components'
 
-const state = reactive<Record<string, any>>({
+const state = reactive<ModalProps>({
+  title: 'Title',
   visible: false,
   width: 800,
+  height: 400,
+  shade: true,
   fullscreen: false,
+  fullscreenEnabled: true,
+  draggable: true,
 })
 </script>
