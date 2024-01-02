51566d435b8e5c330b6a539a0862e4db7ebaff51
momoky <778259229@qq.com>
2024-01-02 17:37:55 +0800

    doc: 更新文档

--- packages/ui/src/components/carousel/stories/carousel.story.vue
+++ packages/ui/src/components/carousel/stories/carousel.story.vue
@@ -12,27 +12,27 @@
   </Story>
 </template>
 <script setup lang="ts">
-import { computed, ref } from 'vue'
-import { invoke, Dnd } from '@vxin/utils'
+import { computed } from 'vue'
+// import { invoke, Dnd } from '@vxin/utils'
 
-const rad = ref(0)
+// const rad = ref(0)
 const style = computed(() => ({
   // transform: `rotateX(-15deg) rotateY(${rad.value}deg)`,
 }))
-const dnd = new Dnd().on('drag', (ev) => {
+/*const dnd = new Dnd().on('drag', (ev) => {
   rad.value += (ev.diff.x / 1000) * 360
   rad.value %= 360
   rad.value = Math.round(rad.value)
-})
-invoke(function render() {
+})*/
+/*invoke(function render() {
   rad.value += 1
   rad.value %= 360
   requestAnimationFrame(render)
-})
-for (let i = 0; i < 6; i++) {
+})*/
+/*for (let i = 0; i < 6; i++) {
   const rad = i * (Math.PI / 3) + Math.PI / 2
   console.log(`translate3d(${Math.round(Math.cos(rad) * 300)}px, 0, ${Math.round(Math.sin(rad) * 300)}px)`)
-}
+}*/
 </script>
 
 <style scoped lang="scss">
@@ -61,7 +61,7 @@ for (let i = 0; i < 6; i++) {
       @keyframes plane-rotate {
         //@each $index in (0, 1, 2, 3, 4, 5) {
         //  #{$index * 20}% {
-        //    transform: rotateY(-#{$index * 72}deg);
+        //    transform: rotateY(-#{$index * 60}deg);
         //  }
         //}
         0% {
@@ -87,17 +87,43 @@ for (let i = 0; i < 6; i++) {
         @each $index in (1, 2, 3, 4, 5, 6) {
           &:nth-child(#{$index}) {
             //transform: rotateY(#{($index - 1) * 60}deg) translateZ(300px);
-            animation: self-action-#{$index} 10s infinite;
+            //animation: self-action-#{$index} 10s infinite;
+            display: none;
 
-            @keyframes self-action-#{$index} {
-              0% {
-                //transform: scaleZ(2) rotateY(#{($index - 1) * 60}deg) translateZ(300px);
-                transform: translateZ(300px) scaleZ(2) rotateY(#{($index - 1) * 60}deg);
-              }
-              100% {
-                //transform: scaleZ(2) rotateY(#{($index - 1) * 60 - 360}deg) translateZ(300px);
-                transform: translateZ(300px) scaleZ(2) rotateY(#{($index - 1) * 60 - 360}deg);
-              }
+            //@keyframes self-action-#{$index} {
+            //  0% {
+            //    //transform: scaleZ(2) rotateY(#{($index - 1) * 60}deg) translateZ(300px);
+            //    transform: scaleZ(2) translate3d(0, 0, 300px) rotateY(#{($index - 1) * 60}deg);
+            //  }
+            //  100% {
+            //    //transform: scaleZ(2) rotateY(#{($index - 1) * 60 - 360}deg) translateZ(300px);
+            //    transform: scaleZ(2) translate3d(0, 0, 300px) rotateY(#{($index - 1) * 60 - 360}deg);
+            //  }
+            //}
+          }
+        }
+
+        &:nth-child(1) {
+          display: block;
+          animation: self-action-1 10s infinite;
+          @keyframes self-action-1 {
+            0% {
+              transform: scaleZ(2) translate3d(0, 0, 300px) rotateY(0deg);
+            }
+            100% {
+              transform: scaleZ(2) translate3d(0, 0, 300px) rotateY(-360deg);
+            }
+          }
+        }
+        &:nth-child(2) {
+          display: block;
+          animation: self-action-2 10s infinite;
+          @keyframes self-action-2 {
+            0% {
+              transform: scaleZ(2) translate3d(-300px, 0, 0) rotateY(0deg);
+            }
+            100% {
+              transform: scaleZ(2) translate3d(-300px, 0, 0) rotateY(-360deg);
             }
           }
         }
