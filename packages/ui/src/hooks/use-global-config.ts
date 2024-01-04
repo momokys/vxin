7ef644c2ca002dd02dcd2f73e70d9b7b1a7afb50
momoky <778259229@qq.com>
2024-01-04 17:42:22 +0800

    update: 更新 hooks

--- packages/ui/src/hooks/use-global-config.ts
+++ packages/ui/src/hooks/use-global-config.ts
@@ -1,9 +1,6 @@
 import { globalConfig, GlobalConfig } from '@/utils'
 import { computed, ComputedRef } from 'vue'
-import { isNil } from '@vxin/utils'
 
 export const useGlobalConfig = <T extends keyof GlobalConfig>(key: T, fallback?: ComputedRef<GlobalConfig[T]>) => {
-  return computed(() => (isNil(globalConfig[key]) ? fallback?.value : globalConfig[key])) as ComputedRef<
-    GlobalConfig[T]
-  >
+  return computed(() => globalConfig[key] ?? fallback?.value) as ComputedRef<GlobalConfig[T]>
 }
